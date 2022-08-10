45dbde31e8bc5ffe0352b57508c58607b5f06da8
“lijiesheng” <ljs_hsm@163.com>
2022-08-10 13:24:33 +0800

    topic

---
+++ topic/sub2/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "rabbitmq/lib"
+
+/**
+ * @Description
+ * @Author lijiesheng
+ * @Date 2022/1/24 5:49 下午
+ **/
+func main() {
+	kutengOne := lib.NewRabbitMQTopic("exKutengTopic", "*.*.yellow")
+	kutengOne.RecieveTopic()
+}
